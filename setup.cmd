b83b6eb790fdcdaa0bd915335259ec24f441b42e
davidpodhola <david.podhola@naseukoly.cz>
2015-11-28 00:46:13 +0100

    trying to make asp.net 5 buildable on appveyor

---
+++ setup.cmd
@@ -0,0 +1,18 @@
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+
+rem choco install nodejs -y
+
+call npm install -g bower
+
+call npm install
+call bower install
+
+call %userprofile%\.dnx\bin\dnvm install 1.0.0-rc1
+call %userprofile%\.dnx\bin\dnvm upgrade
+call %userprofile%\.dnx\bin\dnvm update-self
+call %userprofile%\.dnx\bin\dnvm list
+call %userprofile%\.dnx\bin\dnvm use 1.0.0-rc1
+call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1\bin\dnu restore
+
+cd servers\frontend\aspnet5\FSharp.ProjectTemplate.Web
+call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1\bin\dnu restore
