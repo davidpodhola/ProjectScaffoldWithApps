e9aa88a664ef07ddb2faca7096cdfeb50bb87be2
davidpodhola <david.podhola@naseukoly.cz>
2015-11-28 00:57:47 +0100

    -final added to rc1

--- setup.cmd
+++ setup.cmd
@@ -1,17 +1,17 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 
-choco install nodejs -y
+rem choco install nodejs -y
 
 call npm install -g bower
 
-call %userprofile%\.dnx\bin\dnvm install 1.0.0-rc1
+call %userprofile%\.dnx\bin\dnvm install 1.0.0-rc1-final
 call %userprofile%\.dnx\bin\dnvm upgrade
 call %userprofile%\.dnx\bin\dnvm update-self
 call %userprofile%\.dnx\bin\dnvm list
-call %userprofile%\.dnx\bin\dnvm use 1.0.0-rc1
-call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1\bin\dnu restore
+call %userprofile%\.dnx\bin\dnvm use 1.0.0-rc1-final
+call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1-final\bin\dnu restore
 
 cd servers\frontend\aspnet5\FSharp.ProjectTemplate.Web
 call npm install
 call bower install
-call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1\bin\dnu restore
+call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1-final\bin\dnu restore
