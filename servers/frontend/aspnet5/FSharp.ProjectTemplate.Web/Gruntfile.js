07c71b452744bcda29972ee40dbe0401cb27447a
davidpodhola <david.podhola@naseukoly.cz>
2015-11-28 02:08:25 +0100

    grunt run before build

--- servers/frontend/aspnet5/FSharp.ProjectTemplate.Web/Gruntfile.js
+++ servers/frontend/aspnet5/FSharp.ProjectTemplate.Web/Gruntfile.js
@@ -1,3 +1,4 @@
+/// <binding BeforeBuild='debugbuild' />
 /*
   This file in the main entry point for defining grunt tasks and using grunt plugins.
   Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
@@ -17,4 +18,9 @@ module.exports = function (grunt) {
             },
         },
     });
+    
+    grunt.registerTask('debugbuild', [
+		'copy'
+    ]);
+    
 };
