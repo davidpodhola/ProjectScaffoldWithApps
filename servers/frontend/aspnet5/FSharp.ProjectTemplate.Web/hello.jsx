6b8bf99dcad781bb75addc22a84438983f44dff7
davidpodhola <david.podhola@naseukoly.cz>
2015-11-28 17:29:26 +0100

    working example with a button

--- servers/frontend/aspnet5/FSharp.ProjectTemplate.Web/hello.jsx
+++ servers/frontend/aspnet5/FSharp.ProjectTemplate.Web/hello.jsx
@@ -1,16 +1,27 @@
 var HelloBox = React.createClass({
   getInitialState: function() {
-    return {value: 'Hello!'};
+    return {firstName: 'first name', lastName: 'last name', hello: 'Press the button'};
   },  
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
+  handleChangedFirstName: function(event) {
+    this.setState({firstName: event.target.value});     
   },  
+  handleChangedLastName: function(event) {
+    this.setState({lastName: event.target.value});
+  },
+  handleSubmit: function(event) {
+    console.log( this.state.firstName + ' ' + this.state.lastName )     
+  },  
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log( prevState.firstName + ' ' + prevState.lastName );
+  },
   render: function() {
     return (
       <div className="helloBox">
         Ask when we saw each other for the last time.<br/>
-        First name:<input type="text" value={this.value} onChange={this.handleChange} /><br/>     
-        Last name:<input type="text" value={this.value} onChange={this.handleChange} /><br/>  
+        First name:<input type="text" value={this.firstName} onChange={this.handleChangedFirstName} /><br/>     
+        Last name:<input type="text" value={this.lastName} onChange={this.handleChangedLastName} /><br/>  
+        {this.hello}
+        <button type="button" className="btn btn-primary btn-block" onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
