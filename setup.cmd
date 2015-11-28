44174c82fe0a9c4f4ea83c20ef521a0195d91fee
davidpodhola <david.podhola@naseukoly.cz>
2015-11-28 01:12:24 +0100

    bower files added
    correctly going back after setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -15,3 +15,4 @@ cd servers\frontend\aspnet5\FSharp.ProjectTemplate.Web
 call npm install
 call bower install
 call %userprofile%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1-final\bin\dnu restore
+cd ..\..\..\..\
