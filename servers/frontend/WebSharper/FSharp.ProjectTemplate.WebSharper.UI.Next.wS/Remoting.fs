40700fc3a68eee717114db21ee5a974a674edd0d
davidpodhola <david.podhola@naseukoly.cz>
2015-11-30 11:04:52 +0100

    FSharp.ProjectTemplate.WebSharper.UI.Next.wS started; packages still in packages.config

---
+++ servers/frontend/WebSharper/FSharp.ProjectTemplate.WebSharper.UI.Next.wS/Remoting.fs
@@ -0,0 +1,12 @@
+namespace FSharp.ProjectTemplate.WebSharper.UI.Next.wS
+
+open WebSharper
+
+module Server =
+
+    [<Rpc>]
+    let DoSomething input =
+        let R (s: string) = System.String(Array.rev(s.ToCharArray()))
+        async {
+            return R input
+        }
