e3953e79e60a25dd30161e925340186b73b77bbd
David Podhola <david@postsharp.net>
2015-12-28 14:38:31 +0100

    BDD tests fixed

--- tests/FSharp.ProjectTemplate.BDDTests/GreetingStepDefinitions.fs
+++ tests/FSharp.ProjectTemplate.BDDTests/GreetingStepDefinitions.fs
@@ -2,12 +2,6 @@
 
 open TickSpec
 open NUnit.Framework
-
-let [<Given>] ``a customer buys a black jumper`` () = 
-    ()
-      
-let [<Given>] ``I have (.*) black jumpers left in stock`` (n:int) =  
-    ()
       
 let [<When>] ``I meet (.*)`` (someone:string) =  
     ()
